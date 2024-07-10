9e5e1e443254d476d1900b1ccc69fc684c0b7dd9
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2024-07-10 17:30:03 +0100

    ✅ - Publish v3 -> OK

--- server.js
+++ server.js
@@ -8,6 +8,4 @@ const server = http.createServer(app);
 conn.sync().then(() => {
     server.listen(port);
     console.log('foi iniciado na porta: ' + port);
-   }).catch((err) => console.error(err)); 
-
-   //Para garantir que todos os processos do Node.js sejam encerrados quando o server é encerrado
+   }).catch((err) => console.error(err));
