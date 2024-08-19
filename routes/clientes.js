586380545a1983aefaa0a0e977791ec7d9c5392f
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-08-19 16:29:52 -0300

    fix: rota para pegar o id do cliente

--- routes/clientes.js
+++ routes/clientes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ClienteController = require("../controllers/clientesController");
 
 router.get("/", ClienteController.getClientes);
+router.get("/:id_cliente", ClienteController.getClienteById);
 router.post("/cadastrar", ClienteController.createCliente);
 router.put("/editar/:id_cliente", ClienteController.updateCliente);
 router.delete("/deletar/:id_cliente", ClienteController.deleteCliente);
