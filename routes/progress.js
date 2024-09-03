16dc55d2291589f9c9edceb03789c40efb0ad843
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-09-03 16:55:37 -0300

    fix: adicionando um put ao inves de patch

--- routes/progress.js
+++ routes/progress.js
@@ -5,7 +5,7 @@ const progressaoController = require('../controllers/progressController');
 router.post('/cadastro', progressaoController.criarProgressao);
 router.get('/', progressaoController.buscarTodasProgressoes);
 router.get('/buscar/:id_user', progressaoController.buscarProgressaoPorId);
-router.patch('/editar/', progressaoController.atualizarProgressao);
+router.put('/editar/', progressaoController.atualizarProgressao);
 router.delete('/delete/:id', progressaoController.deletarProgressao);
 
 module.exports = router;
